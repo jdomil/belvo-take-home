8f4cc8e1989f9c6bd59e85dbbd423b3642011461
jdomil <j.dominguezllata@gmail.com>
2022-11-25 12:01:37 +0100

    Fixing deployment

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/balances', async (req, res) => {
 });
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'producction') {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
